e66d52cc012843b1b45f164f9fc3c9bf8f8312ad
의연 김 <naturalk@kyobobook.com>
2023-05-17 18:54:51 +0900

    feat: setup vite basename

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // https://vitejs.dev/config/shared-options.html#base
+  // https://vitejs.dev/guide/build.html#public-base-path
+  // https://vitejs.dev/guide/build.html#advanced-base-options
+  base: '/tictactoe'
 })
