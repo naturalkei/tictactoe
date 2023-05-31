34f0e8c80a5db883b0bea6ce25c33bd67009ebc6
의연 김 <naturalk@kyobobook.com>
2023-05-31 19:21:58 +0900

    refactor: App.jsx

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+export default {
+  plugins: {
+    // sass: {},
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Game from './App.jsx'
+import { Game, Header } from './components'
 import './index.scss'
 
 const root = document.getElementById('root')
 createRoot(root).render(
   <StrictMode>
+    <Header />
     <Game clss="tictactoe game" />
   </StrictMode>
 )
