2009d414726b540e739b672eeabe064c7aa1145a
의연 김 <naturalk@kyobobook.com>
2023-06-14 18:00:46 +0900

    feat: add reference scripts

---
+++ reference/constants.js
@@ -0,0 +1,18 @@
+export const GRID_LENGTH = 9
+
+export const MAX_MOVES = 10
+
+export const SPACE_STATE = {
+  PLAYER: 'player_filled',
+  AI: 'ai_filled',
+  EMPTY: 'empty_space'
+}
+
+export const GAME_STATE = {
+  PLAYER_TURN: 'player_turn',
+  AI_TURN: 'ai_turn',
+  PLAYER_WON: 'player_won',
+  AI_WON: 'player_o_won',
+  DRAW: 'game_draw',
+  ERROR: 'game_error'
+}
